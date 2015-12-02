d546a14a802b74bcf7d7a2c61dad260ecac46ba9
Nathan Zhang <nathan.z@hbtagency.com.au>
2015-12-03 09:57:33 +1100

    mobile menu

--- js/project/global/full_height_menu.js
+++ js/project/global/full_height_menu.js
@@ -8,10 +8,17 @@ $("#nav-button").click(function(){
     if(!$("#box-container").hasClass("move-left")){
         $("#leftMobileMenu").addClass("move-left");
         $("#box-container").addClass("move-left");
-    }else{
-        $("#leftMobileMenu").removeClass("move-left");
-        $("#box-container").removeClass("move-left");
+        $("#body-mask").addClass("show");
     }
 });
 
+$("#close-mobile").click(function(){
+    if($("#box-container").hasClass("move-left")){
+        $("#leftMobileMenu").removeClass("move-left");
+        $("#box-container").removeClass("move-left");
+        $("#body-mask").removeClass("show");
+    }
+});
+
+//refer body_mask.js
 
