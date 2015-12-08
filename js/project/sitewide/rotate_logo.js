5ff8f812ca07ec6d6d9c9598f6f96ede90e61b3b
Nathan Zhang <nathan.z@hbtagency.com.au>
2015-12-08 17:16:40 +1100

    menu effect fix

--- js/project/sitewide/rotate_logo.js
+++ js/project/sitewide/rotate_logo.js
@@ -8,6 +8,7 @@ hover_element2.hover(function(){
     if(!rotate2.hasClass('over')){
         rotate2.addClass('over');
     }
+
 });
 
 hover_element2.mouseleave(function(){
@@ -16,7 +17,7 @@ hover_element2.mouseleave(function(){
         window.setTimeout( function () { 
             rotate2.removeClass('over');
             lock = false;
-        }, 40000 );
+        }, 38000 );
         
     }
 });
